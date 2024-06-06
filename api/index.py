6e71270a779402b726fc38ce1508e5538b39aea4
legendArther <36272922+legendArther@users.noreply.github.com>
2024-06-06 17:10:22 +0530

    3

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests # type: ignore
 
 app = Flask(__name__)
 
@@ -9,3 +10,23 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/kucoin/<symbol>')
+def kucoin_price(symbol):
+    url = f'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol={symbol}'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        price = data['data']['price']
+        return jsonify({
+            'symbol': symbol,
+            'price': price
+        })
+    else:
+        return jsonify({
+            'error': 'Failed to fetch data from KuCoin API'
+        }), response.status_code
+
+if __name__ == '__main__':
+    app.run(debug=True)
