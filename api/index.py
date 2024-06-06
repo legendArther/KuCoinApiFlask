4c4328921c436c51259dc386ffaf809dfcab4476
legendArther <36272922+legendArther@users.noreply.github.com>
2024-06-06 12:01:14 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,21 +1,7 @@
-from flask import Flask,request
-#from gradio_client import Client
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
-
-@app.route('/generate')
-def about():  
-    return 'Hello, World!'
-
-
-@app.route('/roop', methods = ['GET','POST'])
-def roop():
-    return 'Hello, World!'
-    
-@app.route('/test')
-def tst():
-    return 'testlog123'
