acce8c617ca9cdcd8fef221f8fc6c1a04769759c
legendArther <36272922+legendArther@users.noreply.github.com>
2024-06-07 14:00:06 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,17 +1,5 @@
 from flask import Flask, jsonify, request
-import ccxt
-#from dotenv import load_dotenv
-#import os
 
-# Load environment variables from .env file
-#load_dotenv()
-import uuid
-import hmac
-import hashlib
-import base64
-import time
-import json
-import requests
 
 app = Flask(__name__)
 
