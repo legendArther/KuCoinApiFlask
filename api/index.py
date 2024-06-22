693666cab022066961c2e4de051ab6a609f243d3
legendArther <36272922+legendArther@users.noreply.github.com>
2024-06-22 23:21:57 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -31,23 +31,35 @@ def otp():
     try:
         myotp = request.args.get('myotp')
         client.session_2fa(OTP=str(myotp))
-        return 'myotp'
+        return myotp
     except Exception as e:
         print("Exception : %s\n" % e)
         return str(e)
     
 @app.route('/buy')
 def buy():
-    order_response = client.place_order(
-        exchange_segment='NSE',  # Example: National Stock Exchange Futures & Options segment
-        product='MIS',  # Specify that it's an options order
-        price='',  # Price at which you want to buy/sell the option
-        order_type='MKT',  # Order type, e.g., 'LIMIT', 'MARKET'
-        quantity= '1',  # Quantity of the options contract
-        validity='DAY',  # Order validity, e.g., 'DAY', 'IOC'
-        trading_symbol='TATASTEEL-EQ',  # Example trading symbol for the option
-        transaction_type='B'  # Transaction type: 'BUY' or 'SELL'
-    )
+    try:
+        order_response = client.place_order(
+            exchange_segment='NSE',  # Example: National Stock Exchange Futures & Options segment
+            product='MIS',  # Specify that it's an options order
+            price='',  # Price at which you want to buy/sell the option
+            order_type='MKT',  # Order type, e.g., 'LIMIT', 'MARKET'
+            quantity= '1',  # Quantity of the options contract
+            validity='DAY',  # Order validity, e.g., 'DAY', 'IOC'
+            trading_symbol='TATASTEEL-EQ',  # Example trading symbol for the option
+            transaction_type='B'  # Transaction type: 'BUY' or 'SELL'
+        )
+        # Check the status of the order response
+        if order_response['stat'] != 'Ok':
+            return jsonify(order_response), 400  # Returning a 400 Bad Request with the error details
+
+        return jsonify({'message': 'Order placed successfully'}), 200
+
+    except Exception as e:
+        error_message = f"Exception on /buy: {str(e)}"
+        print(error_message)
+        return jsonify({'error': error_message}), 500  # Returning a 500 Internal Server Error with the exception message
+
 
     print("Order Response:", order_response)
 if __name__ == '__main__':
