fe61d7b84420bf047947f242cdead42a893ceac1
legendArther <36272922+legendArther@users.noreply.github.com>
2024-06-22 15:20:23 +0530

    yo

--- api/index.py
+++ api/index.py
@@ -10,11 +10,9 @@ from neo_api_client import NeoAPI
 load_dotenv()
 
 app = Flask(__name__)
-
-@app.before_first_request
-def setup_server_session():
+def init_app():
     # Example: Log in to an external service and store the session token
-    
+    print('ok')
     no = "+918839000041"
     pas = "Happy123$"
     ck = "N7RD0Ydol8qBNE22SMffcT3FXpMa"
@@ -23,7 +21,8 @@ def setup_server_session():
     client = NeoAPI(consumer_key=ck, consumer_secret=cs, environment='prod',
                     access_token=None, neo_fin_key=None)
     client.login(mobilenumber=no, password=pas)
-    
+init_app()
+
 @app.route('/')
 def home():
     return 'Hello'
@@ -31,38 +30,6 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
-
-@app.route('/kucoin/<symbol>')
-def kucoin_price(symbol):
-    url = f'https://api.kucoin.com/api/v1/market/orderbook/level1?symbol={symbol}'
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        price = data['data']['price']
-        return jsonify({
-            'symbol': symbol,
-            'price': price
-        })
-    else:
-        return jsonify({
-            'error': 'Failed to fetch data from KuCoin API'
-        }), response.status_code
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    data = request.json
-    close_position()
-    # Extract action, symbol, and amount from the alert
-    action = data.get('action')
-    symbol = data.get('symbol')
-    amount = data.get('amount')
-    leverage = data.get('leverage', 1)
-
-@app.route('/kucoin/close', methods=['POST'])
-def close_position():
-    data = request.json
-    return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
