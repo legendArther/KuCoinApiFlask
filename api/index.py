642e2454d8118865e927cd9c4f768bfa6df3eaf7
legendArther <36272922+legendArther@users.noreply.github.com>
2024-06-25 01:06:48 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -32,8 +32,7 @@ def otp():
     try:
         myotp = request.args.get('myotp')
         client.session_2fa(OTP=str(myotp))
-        get_max_quantity() #comment
-
+     #   get_max_quantity() #comment
         return myotp
     except Exception as e:
         print("Exception : %s\n" % e)
