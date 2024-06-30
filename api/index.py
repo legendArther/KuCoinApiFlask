69912bcfd3ec752114ad683fd24d0d950ac40266
legendArther <36272922+legendArther@users.noreply.github.com>
2024-06-30 12:20:14 +0530

    h

--- api/index.py
+++ api/index.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 
 #print('login')
 
-no = "+918839000041"
-pas = "Happy123$$"
-ck = "N7RD0Ydol8qBNE22SMffcT3FXpMa"
-cs = "OfE3Hxw4QBAj7jSbrYsM5V01EQYa"
+# no = "+918839000041"
+# pas = "Happy123$$"
+# ck = "N7RD0Ydol8qBNE22SMffcT3FXpMa"
+# cs = "OfE3Hxw4QBAj7jSbrYsM5V01EQYa"
 
-client = NeoAPI(consumer_key=ck, consumer_secret=cs, environment='prod',
-                access_token=None, neo_fin_key=None)
-client.login(mobilenumber=no, password=pas)
+# client = NeoAPI(consumer_key=ck, consumer_secret=cs, environment='prod',
+#                 access_token=None, neo_fin_key=None)
+# client.login(mobilenumber=no, password=pas)
 
 
 @app.route('/')
