edc9cc99436755ea73e4ab2835c86e0454e161e0
legendArther <36272922+legendArther@users.noreply.github.com>
2024-07-01 01:39:18 +0530

    6.5

--- api/index.py
+++ api/index.py
@@ -19,7 +19,7 @@ client.login(mobilenumber=no, password=pas)
 
 @app.route('/')
 def home():
-    quantity = test()
+    quantity = get_max_quantity()
     return (quantity)
 
 @app.route('/login')
@@ -62,7 +62,8 @@ def buy():
         return jsonify({'error': error_message}), 500  # Returning a 500 Internal Server Error with the exception message
 
 def order(symb):
-    max_quantity = get_max_quantity()
+    pos = get_positions_quantity()
+    max_quantity = pos if pos >= 1 else get_max_quantity()
     try:
         order_response = client.place_order(
             exchange_segment='nse_cm',
@@ -101,26 +102,23 @@ def get_max_quantity():
         return str(max_quantity)
     except Exception as e:
         return(f"Exception when fetching available cash: {e}")
-def test():
+def get_positions_quantity():
     try:
-        margin_data = client.margin_required(
-            exchange_segment="nse_cm",  # Example segment
-            price="0",  # Dummy price to get margin data
-            order_type="MKT",
-            product="MIS",
-            quantity="1",  # Dummy quantity
-            instrument_token="3499",  # Dummy instrument token, replace with a valid one
-            transaction_type="B",
-        )
-        available_cash = float(margin_data['data']['avlCash'])
-        margin = float(margin_data['data']['totMrgnUsd'])
-        max_quantity = int(available_cash / margin)
-        print(f"Available Cash: {available_cash}")
-        print(f"Total Margin: {margin}")
-        print(f"Maximum Quantity: {max_quantity}")
-        return str(max_quantity)
+        response = client.positions()
+        print("Positions response:", response)
+        totalquantity = 0  # Initialize totalquantity
+        if response and 'data' in response:
+            for position in response['data']:
+                buy_quantity = int(position.get('flBuyQty', 0))
+                sell_quantity = int(position.get('flSellQty', 0))
+                net_quantity = abs(buy_quantity - sell_quantity)
+                totalquantity += net_quantity * 2
+            print(f"Total Quantity from positions: {totalquantity}")
+        return totalquantity
     except Exception as e:
-        return(f"Exception when fetching available cash: {e}")
+        print(f"Exception when calling PositionsApi->positions: {e}")
+        return 0
+
 
 if __name__ == '__main__':
     app.run(debug=True)
